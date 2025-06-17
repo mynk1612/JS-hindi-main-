604a0986104ea8ffb175e0d79748046551ffcdd7
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-17 13:18:20 +0000

    converionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,31 @@
-let score = "33abc"
+let score = true  // variable declare 
 
-console.log(typeof score);
+/*let score = "33" //string 
+let score = 33  //number*/
+
+console.log(typeof score);   // known about type 
 console.log(typeof(score)); //method 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(score)  //number 
+console.log(typeof valueInNumber); //
+console.log(valueInNumber); //special types not an number 
+
+//"33" =>33
+//"33abc" => NaN (not an number )
+// true => 1; false => 0 
+
+//let isLoggedIn = 1  //true
+let isLoggedIn = "" //False
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" =>false
+// "mayank" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
