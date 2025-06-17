34eff968b9ac9d6ae4046df2ed58138425f92f7e
lalitabora <lalitabora1414@gmail.com>
2025-06-17 21:02:02 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,6 +26,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *************************** Operations ******************************************
+
+let value = 3 
+let negValue =-value 
+console.log(negValue);
+
+
 
 
