1f0329905d59f1b683657346fec488e63e1ab82e
lalitabora <lalitabora1414@gmail.com>
2025-06-17 21:57:11 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,7 +34,75 @@ let stringNumber = String(someNumber)
 
 let value = 3 
 let negValue =-value 
-console.log(negValue);
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "mayank"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( 3 + 4 * 5 % 3);
+
+// console.log(true); //trickey conversion 
+// //true
+// console.log(+true); // trickey conversion 
+// //1
+// console.log(true+);
+// //unexpected value
+
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+
+https://tc39.es/ecma262/multipage/abstract-operatiohs.
+html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
