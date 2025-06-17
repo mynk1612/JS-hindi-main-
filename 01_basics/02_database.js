533a62a5fa0ed7c4e748673c303987d74440c7d8
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-17 12:45:30 +0000

    container conflicts

---
+++ 01_basics/02_database.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version 
+
+// alert( 3+3 ) // we are using nodejs ,not browser 
+
+console.log(30+3)// code readability should be high
+console.log("Mayank") 
+
+
+let name = "Manku" // string datatype 
+let age = 18  // number values 
+let isLoggedIn = false  // boolean dataypes 
+let state; // undefined value 
+
+// number => 2 to power 53
+// bigint 
+// string
+// boolean => true/false 
+// null (standalone values)
+// undefined =>
+// symbol => unique 
+// object
+
+console.log(typeof "mayank"); //string
+console.log(typeof undefined); // undefine
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score)); //method 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
