1fe6a662eba740d29275652d9051242e546137a2
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-18 17:44:38 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,45 @@
+const name = "mayank"
+const repoCount = 50 
+
+//console.log(name + repoCount = "Value");
+
+console.log(`hello my name is ${mayank} and my repo count is ${repoCount}`);
+
+const ganeName = new String('mayank') // type in console after that ganeName fill , the string is shown , after that scroll down 
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));  //to check in which area the character are there in (mayank)
+console.log(gameName.indexof('y')); // to check in which index the 'y' is lie on it 
+
+const newString = gameName.substring(0 , 4)
+console.log(newString);
+
+const anotherString = gameName.slice-8 , 4)
+console.log(anotherString);
+
+const newStringOne = " mayank "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url =  "https://mayank.com/mayank%23kandpal"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('mayank'))
+
+
+
+
+
+
+
+
+
+
+
+
 
