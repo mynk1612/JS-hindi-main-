aa83e1c101a9831cc1e44e3ab9553741da3e5997
lalitabora <lalitabora1414@gmail.com>
2025-06-19 13:53:38 +0530

    Update 01_Arrays.js

--- 01_Arrays.js
+++ 01_Arrays.js
@@ -1,1 +1,6 @@
+// Array -  simple programming language, which enable and store a collection of multiple items under a single vairable enables. 
+// 
+const myArr = [0, 1, 2, 3, 4, 5]
+const m
 
+console.log(myArr[0]);
