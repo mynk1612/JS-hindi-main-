48d05563b59f25196737044265a78a74aa1aaa26
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-19 18:35:35 +0530

    Update 02_object_depth.js

--- 02_object_depth.js
+++ 02_object_depth.js
@@ -1,1 +1,56 @@
+// sigleton 
+//Object.create
+
+
+// .....................object literals .............................
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+  name: "Mayank",
+  "full name": "Mayank Kandpal",
+  [mySym]: " mykey1 ",
+  age: 18,
+  location: "Jaipur",
+  email: "mayank@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"]
+  } 
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+
+JsUser.email = "mayank@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "mayank@microsoft.com"
+console.log(JsUser);
+
+
+//added funtion 
+
+JsUser.greeting = funciton(){
+  console.log('Hello Js User ,${this.name}');
+}
+JsUser.greetingTwo = funciton(){
+  console.log('Hello Js User,${this.name}');
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
