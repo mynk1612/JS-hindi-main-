369ba76985c7ff15c54f81ba5676cfc7193bb692
Lalita Bora <lalitabora1414@gmail.com>
2025-06-21 22:25:16 +0530

    Create 03_basics_function.js

---
+++ 03_basics_function.js
@@ -0,0 +1,82 @@
+//console.log("M");
+
+
+function sayMyName() {
+    console.log("M");
+    console.log("A");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+    console.log("K");
+}
+
+// sayMyName ()
+
+//function addTwoNumbers(number1, Number2){ // Parameters 
+//    console.log(number1 + Number2);
+//}
+
+//addTwoNumbers(3, "a") // function call whatever value is given to call cld as argument 
+
+//addTwoNumbers(3, null)
+
+
+function addTwoNumbers(number1, number2){
+
+   // let result = number1 + number2
+   // return result 
+
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: " , result);
+
+
+//**************login user *******
+
+function loginUserMessage(username){
+    if(username === undefined){
+    console.log("Please enter a username");
+    return 
+    }
+    return `${username} just logged in`
+}
+
+//console.log(loginUserMessage("mayank"))
+//console.log(loginUserMessage())
+
+
+
+function calculateCartPrice(){
+return num 1
+}
+
+console.log(calculateCartPrice(200 400 ,500, 2000))
+
+const user {
+    username: "mayank",
+    price: 151 
+}
+
+
+function handleObject(anyobject){
+        console.log(`Username is ${anyobject.username)anyobject.userName)and priced is $ {anyobject.price}`);
+    }
+
+    // handleoObject(user)
+    handleObject({
+        username: "kis",
+        price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+functions returnSecondValue(getArray){
+
+    return getArray[1]
+
+}
+//console.log(returnSecondValue)myNewArrau));
+console.log(returnSecondValue(["200, 400, 600"]
