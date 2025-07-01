4e3a129a9bf3bfd3234302ae7ced03eb12dd7968
Lalita Bora <lalitabora1414@gmail.com>
2025-07-01 10:04:30 +0530

    Create 03_arrows.js

---
+++ 03_arrows.js
@@ -0,0 +1,14 @@
+const user = {
+  username: "mayank"
+  price: 999,
+
+  welcomeMessage: function() {
+    console.log(`${this.username } , welcome to website`);
+  }
+  
+}
+
+user.welcomeMessage () // default value 
+user.username = "sam"
+
+
