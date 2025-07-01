e4645d6be14e6d5083698cac9b6dc72dc921842d
Lalita Bora <lalitabora1414@gmail.com>
2025-07-01 12:18:49 +0530

    Update 03_arrows.js

--- 03_arrows.js
+++ 03_arrows.js
@@ -4,11 +4,66 @@ const user = {
 
   welcomeMessage: function() {
     console.log(`${this.username } , welcome to website`);
+    console.log(this);
+  
   }
   
 }
 
-user.welcomeMessage () // default value 
-user.username = "sam"
+// user.welcomeMessage () // default value 
+// user.username = "sam"
+// user.welcomeMessage()
+
+//console.log(this);  {} empty object show in output in node 
+
+// function chai(){
+//   let username = "mayank"
+//   console.log(this);
+//   console.log(this.username); //undefined
+// }
+
+// chai()
 
 
+// *********************************************ARROW FUNCTION*************************************************
+
+// const chai = function () {
+//   let username = "mayank"
+//   console.log(this.username);
+// }     // undefined o/p 
+  
+const chai = () => { // arrow funtion () => 
+  let username = "mayank"
+  console.log(this);
+}
+
+chai() 
+
+
+/* ++++++++++++++++++++++++++++++++++++ ARROW FUNCTION +++++++++++++++++++++++++++++++++++++++  
+      *********************** BASIC ARROW FUNCTION **************************** */ 
+
+/*                    
+
+// EXPLICIT RETURN 
+const addTwo =(num1, num2) => {
+  return num1 + num2
+}   
+*/
+
+/* Implicit RETURN */  // in this the paranthesis are remove and return also remove from code 
+
+//const addTwo =(num1, num2) =>  num1 + num2
+// const addTwo =(num1, num2) =>  ( num1 + num2)
+ 
+/* object return */
+const addTwo =(num1, num2) =>  ({username: "mayank"}) // mayank
+const addTwo =(num1, num2) =>  {username: "mayank"}  // undefined 
+
+console.log(addTwo(3, 4))  // OUTPUT 7 
+
+//const myArray = [2, 5, 3,7,8]
+// myArray.forEach()
+
+
+  
