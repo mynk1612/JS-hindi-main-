607ad3df74ea99f1b0ef47f96a731ecbc8ef2ee3
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-07-01 13:29:08 +0530

    Create IIFE.js

---
+++ IIFE.js
@@ -0,0 +1,86 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE 
+    console.log(`DB CONNECTED`);
+
+})();
+// global scope ke 
+
+(function aurcode(name ) {
+
+    console.log(`DB CONNECTED TWO ${name}`); // varaible name
+
+})('mayank');
+
+( (name ) => {
+    
+    console.log(`DB CONNECTED Three ${name}`); // varaible name
+
+})('mayank')                                                                    
+
+
+
+/* +++++++++++++++++ JAVASCRIPT EXECUTION CONTEXT +++++++++++ 
+
+for execution firhtly Global Execution context run in this we learn  (this)
+ 
+Javascript is single threaded 
+
+Three types of Execution context 
+
+1- Global Execution Context
+2- Function Execution Context
+3- Eval Execution Context 
+
+java script code run, it is run in two phase 
+
+1- Memory Cretion Phase  // only memory allocation phase done in this 
+2- Execution phase  // 
+
+Code file for memory creation phase 
+
+let val1 = 10
+let val2 = 5 
+function addNum(num1, num2){
+    let total = num1 +num2
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
++++++++++++++++Steps for Run the Code++++++++++++++++++++++++ 
+
+1 Global Execution to this 
+
+2 Memory Phase or Creation Phase (all variable collect---- 
+       
+        val1- undefined, 
+        val2- undefined
+        addnum - defination)
+        result1- undefined 
+        reuslt2- undefined 
+    //first cycle 
+
+3 Execution Phase 
+
+    val1 - 10
+    val2 - 5
+    addNum - newvariable environment + Execution thread 
+
+    after this addnum memory phase again comes 
+
+    Memory Phase 
+            val1- undefined, 
+            val2- undefined 
+            total - undefined 
+        
+    Execution Context 
+            num1 - 10
+            num2 - 5
+
+            total - 15
+
+
+*/
+
